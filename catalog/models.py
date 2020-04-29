a778185f335d616147acaf145512306b415bdd65
Densolator <raffaeld12@gmail.com>
2020-04-29 17:34:29 +0800

    All borrowed books and renew functionality for library staff done.

--- catalog/models.py
+++ catalog/models.py
@@ -68,6 +68,8 @@ class BookInstance(models.Model):
 		help_text = 'Book Availability',
 	)
 	
+	
+
 	@property
 	def is_overdue(self):
 		if self.due_back and date.today() > self.due_back:
@@ -76,6 +78,7 @@ class BookInstance(models.Model):
 
 	class Meta:
 		ordering = ['due_back']
+		permissions = (("can_mark_returned", "Set book as returned"),)
 		
 	def book_language(self):
 		return "\n".join([str(p) for p in self.language.all()])
--- catalog/urls.py
+++ catalog/urls.py
@@ -12,3 +12,10 @@ urlpatterns = [
 urlpatterns += [   
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
+urlpatterns += [   
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [   
+    path('borrowed/', views.BookInstanceListView.as_view(), name='borrowed-books'),
+]
